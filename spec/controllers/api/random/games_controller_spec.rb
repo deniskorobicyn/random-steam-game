f002d07d69a2e1cb1d6a066d13d9cfb8d6e9394d
Denis Korobicyn <deniskorobitcin@gmail.com>
2018-01-04 00:04:51 +0500

    Fix tests

--- spec/controllers/api/random/games_controller_spec.rb
+++ spec/controllers/api/random/games_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Api::Random::GamesController, type: :controller do
+RSpec.describe Api::V1::Random::GamesController, type: :controller do
   let(:user) { create :user }
 
   before do
