025a3ee102d0ed96ab213d353493bdf9fdc70c97
Denis Korobicyn <deniskorobitcin@gmail.com>
2018-01-06 21:58:15 +0500

    Fix tests part 3

--- spec/interactors/randomize_game_spec.rb
+++ spec/interactors/randomize_game_spec.rb
@@ -6,7 +6,7 @@ describe RandomizeGame do
     let!(:game) { create :game, users: [user] }
 
     before do
-      allow(Steam::UserStats).to receive(:game_schema).with(320).and_return({})
+      allow(Steam::UserStats).to receive(:game_schema).with(game.steam_appid).and_return({})
     end
 
     it 'adds to context game, image and appid' do
